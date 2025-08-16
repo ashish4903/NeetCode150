2c227a1e7df42ac664048463badd4dd78ecdd6fd
Ashish Bandiwar <abandiwar@svam.com>
2025-08-15 23:23:06 -0400

    Added changes for Encode & Decode

---
+++ EncodeDecode/EncodeDecode/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace EncodeDecode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IList<string> list = new List<string>();
+            list.Add("Neet");
+            list.Add("Co#de");
+            Program program = new Program();
+            Console.WriteLine(program.Encode(list));
+            Console.WriteLine(program.Decode(program.Encode(list)));
+            Console.WriteLine("Hello, World!");
+        }
+
+        public string Encode(IList<string> strs)
+        {
+            string res = "";
+            foreach (string str in strs)
+            {
+                res += str.Length + "#" + str;
+            }
+            return res;
+
+        }
+
+        public string Decode(string str)
+        {
+            List<string> res = new List<string>();
+
+            int i = 0;
+
+            while(i < str.Length)
+            {
+                int j = i;
+
+                while (str[j] != '#')
+                {
+                    j++;
+                }
+
+                int length = int.Parse(str.Substring(i,j-i));
+
+                i = j + 1;
+                j= i + length;
+                res.Add(str.Substring(i,length));
+                i = j;
+            }
+
+            return res.ToString();
+
+        }
+    }
+}
