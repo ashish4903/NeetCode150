b377f2521fce5b3fa7c26221761a544c5e67337c
Ashish Bandiwar <abandiwar@svam.com>
2025-08-16 23:48:21 -0400

    Added changes for the optimized version

--- ProductExceptSelf/ProductExceptSelf/Program.cs
+++ ProductExceptSelf/ProductExceptSelf/Program.cs
@@ -10,6 +10,7 @@
             Console.WriteLine("Hello, World!");
         }
 
+        /*
         /// <summary>
         /// Time Complexity: O(n^2) Space Complexity: O(1)
         /// </summary>
@@ -19,19 +20,46 @@
         {
             int[] result = new int[nums.Length];
 
-            for(int i = 0; i < nums.Length; i++)
+            for (int i = 0; i < nums.Length; i++)
             {
                 int product = 1;
-                for(int j = 0; j < nums.Length; j++)
+                for (int j = 0; j < nums.Length; j++)
                 {
-                    if(i!=j)
+                    if (i != j)
                     {
-                        product= product * nums[j];
+                        product = product * nums[j];
                     }
                 }
                 result[i] = product;
             }
-            return result;          
+            return result;
+        }
+        */
+
+        /// <summary>
+        /// Time Complexity: O(n) Space Complexity: O(1)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+
+            Array.Fill(res, 1);
+
+            for(int i = 1; i < nums.Length; i++)
+            {
+                res[i] = res[i - 1] * nums[i - 1];
+            }
+
+            int postfix = 1;
+
+            for(int i = nums.Length-1; i >= 0; i--)
+            {
+                res[i] = res[i] * postfix;
+                postfix *= nums[i];
+            }
+            return res;
         }
     }
 }
