465a82a8a884b551e2ab7536f10ccb973c39d75b
Ashish Bandiwar <abandiwar@svam.com>
2025-08-16 23:22:41 -0400

    Added changes for product of array excelf self

---
+++ ProductExceptSelf/ProductExceptSelf/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ProductExceptSelf
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            int[] nums = { 1, 2, 3 };
+            Console.WriteLine(program.ProductExceptSelf(nums));
+            Console.WriteLine("Hello, World!");
+        }
+
+        /// <summary>
+        /// Time Complexity: O(n^2) Space Complexity: O(1)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                int product = 1;
+                for(int j = 0; j < nums.Length; j++)
+                {
+                    if(i!=j)
+                    {
+                        product= product * nums[j];
+                    }
+                }
+                result[i] = product;
+            }
+            return result;          
+        }
+    }
+}
