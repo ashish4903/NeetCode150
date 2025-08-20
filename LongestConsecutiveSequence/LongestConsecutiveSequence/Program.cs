ef1e238ea09a68a0650f91ad4883d7a82e955708
Ashish Bandiwar <abandiwar@svam.com>
2025-08-19 22:18:52 -0400

    Added changes for the Longest Consectuive Sequence optimized solution

--- LongestConsecutiveSequence/LongestConsecutiveSequence/Program.cs
+++ LongestConsecutiveSequence/LongestConsecutiveSequence/Program.cs
@@ -6,12 +6,13 @@
         {
             Console.WriteLine("Program Execution Start");
             Program program = new Program();
-            int[] nums = { 1, 2, 3, 4, 5, 6, 0, 12, 56 };
+            int[] nums = { 1, 1, 2, 3, 4, 5, 6, 0, 12, 56 };
             Console.WriteLine(program.LongestConsecutive(nums));
             Console.WriteLine("Program Execution End");
             Console.WriteLine("Hello, World!");
         }
 
+        /*
         /// <summary>
         /// Time Complexity: O(n^2) Space Complexity: O(n) 
         /// </summary>
@@ -33,6 +34,33 @@
                 res = Math.Max(res, streak);
             }
             return res;
+        }
+        */
+
+        /// <summary>
+        /// Time Complexity: O(n) Space Complexity: O(n) 
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int LongestConsecutive(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+
+            int longest = 0;
+
+            foreach (int num in set)
+            {
+                if(!set.Contains(num-1))
+                {
+                    int length = 1;
+                    while(set.Contains(num+length))
+                    {
+                        length++;
+                    }
+                    longest = Math.Max(longest, length);
+                }
+            }
+            return longest;
         }
     }
 }
